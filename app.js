0128abe735cf79beb15ea44d834a4df2e028c886
kumprji1 <56874614+kumprji1@users.noreply.github.com>
2023-06-06 17:36:18 +0200

    typo

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(express.static(path.join('public')))
 // Stripe Listener
 app.post('/webhook', bodyParser.raw({type: 'application/json'}), async (request, response) => {
     console.log('Objednávka zaplacena')
-    const payload = request.rawBody;
+    const payload = request.body;
     console.log('paylaod: ', payload)
     const sig = request.headers['stripe-signature'];
     console.log('signature: ', sig)
